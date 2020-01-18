ef65f2db908ec2586f241ecb3e3e8909a5bf28e2
CrazyCanineStudios <32432850+CrazyCanineStudios@users.noreply.github.com>
2020-01-18 22:36:22 +0000

    Implemented Basic Main Menu

--- Survival_Shooter/js/Main.js
+++ Survival_Shooter/js/Main.js
@@ -1,23 +1,24 @@
 var config = {
-  width: 1920,
-  height: 1080,
-  backgroundColor: 0x000000,
-  scene: [Boot, Level_1],
-  pixelArt: true,
-  // 1.1 set the physics to arcade
-  physics: {
-    default: "arcade",
-    arcade:{
-        debug: false
-    }
-  }
-}
+        type: Phaser.AUTO,
+        width: 1920,
+        height: 1080,
+        backgroundColor: 0x000000,
+      scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+      },
+        scene: [Boot, MainMenu, Level_1],
+        pixelArt: false,
+        // 1.1 set the physics to arcade
+        physics: {
+            default: "arcade",
+            arcade: {
+                debug: false
+            }
+        }
+    }, player, moveKeys, reticle, bullets, lastFired, moving, time, music,confirmSound,
+    game = new Phaser.Game(config);
 
-var player;
-var moveKeys;
-var reticle;
-var bullets;
-var lastFired;
-var moving;
-var time;
-var game = new Phaser.Game(config);
+
--- Survival_Shooter/js/states/Boot.js
+++ Survival_Shooter/js/states/Boot.js
@@ -3,18 +3,24 @@ class Boot extends Phaser.Scene {
     super("bootGame");
   }
 
-  preload(){
+  preload()
+  {
+    console.log("Boot State Started");
+    this.load.image('menuImage','assets/sprites/menu.png');
     this.load.image("background", "assets/sprites/backgrounds/bg_level_1.png");
     //
     this.load.image("crosshair", "assets/sprites/spr_crosshair.png");
-    this.load.spritesheet("player", "assets/sprites/spr_player_girl.png",{
-      frameWidth: 204,
-      frameHeight: 265
-    });
+    this.load.spritesheet("player", "assets/sprites/spr_player_girl.png",{frameWidth: 204, frameHeight: 265});
+    this.load.spritesheet("buttons", "assets/sprites/buttons.png",{frameWidth: 736, frameHeight: 276});
+    this.load.spritesheet("backButtons", "assets/sprites/back_button.png",{frameWidth: 55, frameHeight: 24});
+    this.load.spritesheet("continueButton", "assets/sprites/continue_button.png",{frameWidth: 195, frameHeight: 66});
+    this.load.spritesheet("continueStoryButton", "assets/sprites/continueStoryButton.png",{frameWidth: 84, frameHeight: 24});
+    this.load.audio('titleMusic', ['assets/sounds/music/mus_title.ogg']);
+    this.load.audio('confirmSound', ['assets/sounds/sound effects/snd_confirm.ogg']);
   }
   create() {
     this.add.text(20, 20, "The game is loading");
-    this.scene.start("playGame");
+    this.scene.start("mainMenu");
   }
   update () {
 
